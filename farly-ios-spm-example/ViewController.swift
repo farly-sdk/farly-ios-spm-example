0b83e0f3411cecb36a08c8ed759a29820e51472c
Philippe Auriach <p.auriach@gmail.com>
2022-08-22 15:58:49 +0200

    Initial Commit

---
+++ farly-ios-spm-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  farly-ios-spm-example
+//
+//  Created by Philippe Auriach on 22/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
